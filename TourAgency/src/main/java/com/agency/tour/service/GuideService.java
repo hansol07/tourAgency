4b56e4baf70c26f0b0936ccb6ed7f41f9a82dd7a
hansol <njwjsgksthf@naver.com>
2022-10-28 10:36:28 +0900

    feat : GUIDE SERVICE

--- TourAgency/src/main/java/com/agency/tour/service/GuideService.java
+++ TourAgency/src/main/java/com/agency/tour/service/GuideService.java
@@ -1,8 +1,12 @@
 package com.agency.tour.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.agency.tour.domain.GuideVo;
+import com.agency.tour.global.ActiveEnum;
 import com.agency.tour.repository.GuideRepository;
 
 @Service
@@ -10,5 +14,7 @@ public class GuideService {
 	@Autowired
 	private GuideRepository gRepo;
 	
-	
+	public List<GuideVo> getGuideList(){
+		return gRepo.findAllByIsActive(ActiveEnum.Y.toString());
+	}
 }
